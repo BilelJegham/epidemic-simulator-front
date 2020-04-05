647c3b5cb902eed45460c635756ae57ee071754e
BilelJegham <bilel@jegham.com>
2020-04-05 16:12:34 +0200

    Fix Update

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -21,8 +21,9 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; please refresh.')
-      let worker = registration.waiting
-      worker.postMessage({action: 'skipWaiting'})
+      setTimeout(() => {
+        window.location.reload(true)
+      }, 1000)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
